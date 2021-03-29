f5541f85fede5e5f9e3d99b419601ee2fd1101ad
Lisander Doda <lisander_doda@yahoo.it>
2021-03-29 22:47:18 +0200

    added pettypeservices. closes #14

---
+++ pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.ld.springframework.sfgpetclinic.services;
+
+import com.ld.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
