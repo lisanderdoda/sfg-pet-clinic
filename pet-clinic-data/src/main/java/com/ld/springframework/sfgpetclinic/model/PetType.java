9eb1e23049f1e17992b4352d97e37f7c00f4dbf2
Lisander Doda <lisander_doda@yahoo.it>
2021-03-31 16:42:20 +0200

    update owners to JPA entities. closes #21

--- pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/model/PetType.java
@@ -1,5 +1,10 @@
 package com.ld.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "type")
 public class PetType extends BaseEntity{
 
     private String name;
