e378a41045ac57ca7cd0506b4b3fd42125f72b78
Lisander Doda <lisander_doda@yahoo.it>
2021-03-31 17:10:34 +0200

    updated visit jpa mapping. closes #23

--- pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/model/Visit.java
@@ -1,11 +1,18 @@
 package com.ld.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity{
 
     private LocalDate date;
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
