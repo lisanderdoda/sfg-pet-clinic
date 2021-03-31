0e7962626d9beb34231a3a219755678226127bfe
Lisander Doda <lisander_doda@yahoo.it>
2021-03-31 16:21:07 +0200

    update BaseEntity with JPA annotation. closes #20

--- pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ld/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,14 @@
 package com.ld.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
     public Long getId() {
